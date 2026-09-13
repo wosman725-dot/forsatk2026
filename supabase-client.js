dace9760cd9e6aa2fcc92f001f72f9b2ce870cc8
forsatk <wosman725@gmail.com>
2026-09-13 23:01:50 +0300

    Add Supabase client configuration

---
+++ supabase-client.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ldzllykyxlrturshqixx.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_GVkWKEkugIX7bJMweBXn3w_faoVMKK6";
+
+window.forsatkSupabaseConfig = {
+  url: SUPABASE_URL,
+  anonKey: SUPABASE_ANON_KEY
+};
